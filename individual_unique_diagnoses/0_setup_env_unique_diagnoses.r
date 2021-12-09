c0569abb4c95e3b3f26b97b5ada71091ada7cc5c
Martin Bernstorff <martinbernstorff@gmail.com>
2021-12-09 10:04:54 +0100

    Use "here" for sourcing

--- individual_unique_diagnoses/0_setup_env_unique_diagnoses.r
+++ individual_unique_diagnoses/0_setup_env_unique_diagnoses.r
@@ -4,17 +4,17 @@ library(dplyr, warn.conflicts = FALSE)
 options(lubridate.fasttime = TRUE)
 
 source("E:/Users/adminmanber/Desktop/FeasibilityMapper/utils.r")
-source("../src/functions.r")
-source("../src/individual_unique_diagnoses_functions.r")
-source("../src/ggplot_defaults.r")
+source(here("src", "functions.r"))
+source(here("src", "individual_unique_diagnoses_functions.r"))
+source(here("src", "ggplot_defaults.r"))
 
-df_raw_outpatient <- get_fct("FOR_besoeg_fysiske_fremmoeder") %>% 
+df_raw_outpatient <- get_fct("FOR_besoeg_fysiske_fremmoeder") %>%
   clean_sql_import()
 
-df_raw_lpr3 <- get_fct("FOR_LPR3kontakter_psyk_somatik") %>% 
+df_raw_lpr3 <- get_fct("FOR_LPR3kontakter_psyk_somatik") %>%
   clean_sql_import()
 
-df_raw_lpr2_inpatient <- get_fct("FOR_indlaeggelser_psyk_somatik_LPR2") %>% 
+df_raw_lpr2_inpatient <- get_fct("FOR_indlaeggelser_psyk_somatik_LPR2") %>%
   clean_sql_import()
 
 log_time()
